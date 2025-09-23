ed27a2ebc7f2e73b94e10ade7ae6455004cdfec0
Deemondy144 <mondy144@gmail.com>
2025-09-23 10:25:55 +0100

    commit changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import './index.css'
 import './App.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/Sparkle-Cleaning/'>
+    <Router basename='/Sparkle-Cleaning/'>
     <App />
-    </BrowserRouter>
+    </Router>
   </StrictMode>,
 );
